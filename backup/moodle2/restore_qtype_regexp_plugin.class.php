71c1b1a7d88bd5e518e0a899fb623a38f40847be
rezeau <joseph@rezeau.org>
2025-05-01 19:01:02 +0200

    fix MDL-83541

--- backup/moodle2/restore_qtype_regexp_plugin.class.php
+++ backup/moodle2/restore_qtype_regexp_plugin.class.php
@@ -89,4 +89,18 @@ class restore_qtype_regexp_plugin extends restore_qtype_plugin {
             $this->set_mapping('qtype_regexp', $oldid, $newitemid);
         }
     }
+    #[Override]
+    public static function remove_excluded_question_data(stdClass $questiondata, array $excludefields = []): stdClass {
+        if (isset($questiondata->options->studentshowalternate)) {
+            unset($questiondata->options->studentshowalternate);
+        }
+        if (isset($questiondata->options->usecase)) {
+            unset($questiondata->options->usecase);
+        }
+        if (isset($questiondata->options->usehint)) {
+            unset($questiondata->options->usehint);
+        }
+        return parent::remove_excluded_question_data($questiondata, $excludefields);
+    }
+
 }
